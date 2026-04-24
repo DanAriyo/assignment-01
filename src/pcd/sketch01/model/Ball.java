4209430458b645fdcfedadcfd50c882da8bdfb5f
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-04-24 15:24:20 +0200

    modified the classes Board and Ball to remove every static function

--- src/pcd/sketch01/model/Ball.java
+++ src/pcd/sketch01/model/Ball.java
@@ -6,13 +6,13 @@ public class Ball {
     
     private P2d pos;
     private V2d vel;
-    private double radius;
+    private final double radius;
     private double mass;
-    private Role role;
-    private static Optional<Role> lastHitter = Optional.empty();
+    private final Role role;
+    private Role lastHitter = null;
     
-    private static double FRICTION_FACTOR = 0.25; 	/* 0 minimum */
-    private static double RESTITUTION_FACTOR = 1; 
+    public static double FRICTION_FACTOR = 0.25; 	/* 0 minimum */
+    public static double RESTITUTION_FACTOR = 1;
 
     public Ball(P2d pos, double radius, double mass, V2d vel,Role role){
        this.pos = pos;
@@ -22,28 +22,9 @@ public class Ball {
        this.role = role;
     }
 
-    private void setLastHitter(Role hitter) {
-        lastHitter = Optional.ofNullable(hitter);
+    public void setLastHitter(Role hitter) {
+        lastHitter = hitter;
     }
-
-    private static void updateLastHitter(Ball a, Ball b) {
-
-        if (isStriker(a) && b.getRole() == Role.GENERIC) {
-            b.setLastHitter(a.getRole());
-        }
-        else if (isStriker(b) && a.getRole() == Role.GENERIC) {
-            a.setLastHitter(b.getRole());
-        }
-        else if (a.getRole() == Role.GENERIC && b.getRole() == Role.GENERIC) {
-            a.resetLastHitter();
-            b.resetLastHitter();
-        }
-    }
-
-    private static boolean isStriker(Ball ball) {
-        return ball.getRole() == Role.PLAYER || ball.getRole() == Role.BOT;
-    }
-
 
     public void updateState(long dt, Board ctx){
         double speed = vel.abs();
@@ -58,7 +39,7 @@ public class Ball {
         pos = pos.sum(vel.mul(dt_scaled));
      	applyBoundaryConstraints(ctx);
     }
-    
+
     public void kick(V2d vel) {
     	this.vel = vel;
     }
@@ -86,95 +67,30 @@ public class Ball {
         }
     }
 
-    /**
-     * 
-     * Resolving collision between 2 balls, updating their position and velocity
-     * 
-     * @param a
-     * @param b
-     */
-    public static void resolveCollision(Ball a, Ball b) {
 
-        updateLastHitter(a, b);
-        
-    	/* check if there is a collision */
-    	
-    	/* compute dv = b.pos - a.pos vector */
-
-    	double dx   = b.pos.x() - a.pos.x();
-        double dy   = b.pos.y() - a.pos.y();
-        double dist = Math.hypot(dx, dy);
-        double minD = a.radius + b.radius;
-        
-        /* 
-         * There is a collision if the distance between the two balls is less than the sum of the radii 
-         * 
-         */
-        if (dist < minD && dist > 1e-6)  {
-
-	        /* 
-	         * Collision case - what to do:
-	         * 
-	         * 1) solve overlaps, moving balls 
-	         * 2) update velocities
-	         * 
-	         */
-	        
-        	/* dvn = V2d(nx,ny) = dv unit vector */
-    
-        	double nx = dx / dist;
-	        double ny = dy / dist;
-	
-	        /* 
-	         * 
-	         * Update positions to solve overlaps, moving balls along dvn
-	         * - the displacements is proportional to the mass
-	         * 
-	         */
-	        double overlap = minD - dist;
-	        double totalM  = a.mass + b.mass;
-	
-	        double a_factor = overlap * (b.mass / totalM);
-	        double a_deltax = nx * a_factor; 
-	        double a_deltay = ny * a_factor; 
-	        
-	        a.pos = new P2d(a.getPos().x() - a_deltax, a.getPos().y() - a_deltay);
-	        
-	        double b_factor = overlap * (a.mass / totalM);
-	        double b_deltax = nx * b_factor; 
-	        double b_deltay = ny * b_factor; 
-	
-	        b.pos = new P2d(b.getPos().x() + b_deltax, b.getPos().y() + b_deltay);
-	
-	        /* Update velocities  */
-	        
-	        /* relative speed along the normal vector*/
-	
-	        double dvx = b.vel.x() - a.vel.x();
-	        double dvy = b.vel.y() - a.vel.y(); 
-	        double dvn = dvx * nx + dvy * ny;
-	
-	        if (dvn <= 0) { /* if not already separating, update velocities */
-	        	
-	        	double imp = -(1 + RESTITUTION_FACTOR) * dvn / (1.0/a.getMass() + 1.0/b.getMass());        
-	        	a.vel = new V2d(a.vel.x() - (imp / a.mass) * nx, a.vel.y() - (imp / a.mass) * ny);                
-	        	b.vel = new V2d(b.vel.x() + (imp / b.mass) * nx, b.vel.y() + (imp / b.mass) * ny);
-	        }
-        }
-    }
 
     
     public P2d getPos(){        
     	return pos;
     }
+
+    public void setPos(P2d pos){
+        this.pos = pos;
+    }
     
     public double getMass() {
     	return mass;
+    }
+
+    public void setMass(double mass){
+        this.mass = mass;
     }
     
     public V2d getVel() {
     	return vel;
     }
+
+    public void setVel(V2d vel){ this.vel = vel; }
     
     public double getRadius() {
     	return radius;
@@ -185,11 +101,11 @@ public class Ball {
     }
 
     public Optional<Role> getLastHitter() {
-        return lastHitter;
+        return Optional.ofNullable(lastHitter);
     }
 
-    private void resetLastHitter() {
-        this.lastHitter = Optional.empty();
+    public void resetLastHitter() {
+        this.lastHitter = null;
     }
 
 }
--- src/pcd/sketch01/model/Board.java
+++ src/pcd/sketch01/model/Board.java
@@ -4,6 +4,10 @@ import pcd.sketch01.model.boardConf.BoardConf;
 import pcd.sketch01.util.Pair;
 
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static pcd.sketch01.model.Ball.RESTITUTION_FACTOR;
 
 public class Board {
 
@@ -13,8 +17,11 @@ public class Board {
     private Boundary bounds;
     private Pair<Hole,Hole> holes;
     private final Map<Role, Integer> scores = new EnumMap<>(Role.class);
+    private final Lock mutex;
 
-    public Board(){} 
+    public Board(){
+        this.mutex = new ReentrantLock();
+    }
     
     public void init(BoardConf conf) {
     	balls = conf.getSmallBalls();    	
@@ -25,24 +32,119 @@ public class Board {
         scores.put(Role.PLAYER, 0);
         scores.put(Role.BOT, 0);
     }
+
+    private boolean isStriker(Ball ball) {
+        return ball.getRole() == Role.PLAYER || ball.getRole() == Role.BOT;
+    }
+
+    private void updateLastHitter(Ball a, Ball b) {
+
+        if (isStriker(a) && b.getRole() == Role.GENERIC) {
+            b.setLastHitter(a.getRole());
+        }
+        else if (isStriker(b) && a.getRole() == Role.GENERIC) {
+            a.setLastHitter(b.getRole());
+        }
+        else if (a.getRole() == Role.GENERIC && b.getRole() == Role.GENERIC) {
+            a.resetLastHitter();
+            b.resetLastHitter();
+        }
+    }
+
+    /**
+     *
+     * Resolving collision between 2 balls, updating their position and velocity
+     *
+     * @param a
+     * @param b
+     */
+    public void resolveCollision(Ball a, Ball b) {
+
+        updateLastHitter(a, b);
+
+        /* check if there is a collision */
+
+        /* compute dv = b.pos - a.pos vector */
+
+        double dx   = b.getPos().x() - a.getPos().x();
+        double dy   = b.getPos().y() - a.getPos().y();
+        double dist = Math.hypot(dx, dy);
+        double minD = a.getRadius() + b.getRadius();
+
+        /*
+         * There is a collision if the distance between the two balls is less than the sum of the radii
+         *
+         */
+        if (dist < minD && dist > 1e-6)  {
+
+            /*
+             * Collision case - what to do:
+             *
+             * 1) solve overlaps, moving balls
+             * 2) update velocities
+             *
+             */
+
+            /* dvn = V2d(nx,ny) = dv unit vector */
+
+            double nx = dx / dist;
+            double ny = dy / dist;
+
+            /*
+             *
+             * Update positions to solve overlaps, moving balls along dvn
+             * - the displacements is proportional to the mass
+             *
+             */
+            double overlap = minD - dist;
+            double totalM  = a.getMass() + b.getMass();
+
+            double a_factor = overlap * (b.getMass() / totalM);
+            double a_deltax = nx * a_factor;
+            double a_deltay = ny * a_factor;
+
+            a.setPos(new P2d(a.getPos().x() - a_deltax, a.getPos().y() - a_deltay));
+
+            double b_factor = overlap * (a.getMass() / totalM);
+            double b_deltax = nx * b_factor;
+            double b_deltay = ny * b_factor;
+
+            b.setPos(new P2d(b.getPos().x() + b_deltax, b.getPos().y() + b_deltay));
+
+            /* Update velocities  */
+
+            /* relative speed along the normal vector*/
+
+            double dvx = b.getVel().x() - a.getVel().x();
+            double dvy = b.getVel().y() - a.getVel().y();
+            double dvn = dvx * nx + dvy * ny;
+
+            if (dvn <= 0) { /* if not already separating, update velocities */
+
+                double imp = -(1 + RESTITUTION_FACTOR) * dvn / (1.0/a.getMass() + 1.0/b.getMass());
+                a.setVel(new V2d(a.getVel().x() - (imp / a.getMass()) * nx, a.getVel().y() - (imp / a.getMass()) * ny));
+                b.setVel(new V2d(b.getVel().x() + (imp / b.getMass()) * nx, b.getVel().y() + (imp / b.getMass()) * ny));
+            }
+        }
+    }
     
     public void updateState(long dt) {
 
     	playerBall.updateState(dt, this);
         botBall.updateState(dt,this);
-    	
+
     	for (var b: balls) {
     		b.updateState(dt, this);
-    	}       	
-    	
+    	}
+
     	for (int i = 0; i < balls.size() - 1; i++) {
             for (int j = i + 1; j < balls.size(); j++) {
-                Ball.resolveCollision(balls.get(i), balls.get(j));
+                resolveCollision(balls.get(i), balls.get(j));
             }
         }
     	for (var b: balls) {
-    		Ball.resolveCollision(playerBall, b);
-            Ball.resolveCollision(botBall,b);
+    		resolveCollision(playerBall, b);
+            resolveCollision(botBall,b);
     	}
         balls.removeIf(b -> {
             if (Hole.checkCollision(b, holes.x()) || Hole.checkCollision(b, holes.y())) {
@@ -62,7 +164,7 @@ public class Board {
             System.out.println("GAME SHOULD END");
         }
 
-    	   	    	
+
     }
     
     public List<Ball> getBalls(){
