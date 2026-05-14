f04fb3c0bb6739abf812ad98a4f625d06ba7d539
Oluwatobi Daniel Ariyo - oluwatobi.ariyo@studio.unibo.it <oluwatobi.ariyo@studio.unibo.it>
2026-05-14 15:12:42 +0200

    now enabled to launch from terminal jpf

---
+++ jpf/src/main/java/part01/TestJPF.java
@@ -0,0 +1,12 @@
+package part01;
+
+import gov.nasa.jpf.vm.Verify;
+
+
+public class TestJPF {
+    public static void main(String[] args) {
+        System.out.print("a1");
+        System.out.print("b1");
+
+    }
+}
